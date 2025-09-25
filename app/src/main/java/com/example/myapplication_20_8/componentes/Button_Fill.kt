da53073ef5a0e63cf1d54fba2ac42001a3b2b726
Axel <55245056+Hareyuk@users.noreply.github.com>
2025-09-24 22:02:02 -0300

    Fuente POPPINS importado y utilizado

--- app/src/main/java/com/example/myapplication_20_8/componentes/Button_Fill.kt
+++ app/src/main/java/com/example/myapplication_20_8/componentes/Button_Fill.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.myapplication_20_8.PoppinsFamily
 
 @Composable
 fun BotonVerde(
@@ -24,6 +25,6 @@ fun BotonVerde(
             containerColor = Color(0xFF4CAF50)
         )
     ) {
-        Text(texto)
+        Text(texto, fontFamily = PoppinsFamily)
     }
 }
